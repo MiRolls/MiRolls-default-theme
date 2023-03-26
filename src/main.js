1102f0ec8a880dc39e3b70baa0de83bf9f4de5a5
liangmiQwQ <mi_yyds@126.com>
2023-03-26 11:22:22 +0800

    fix: fix can't render while fetch

--- src/main.js
+++ src/main.js
@@ -2,39 +2,85 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import './index.css'
 import VueCookies from 'vue-cookies'
-import zh from "./assets/langs/zh";
-import en from "./assets/langs/en";
 import {createI18n} from "vue-i18n";
-import router from "./routerSetting"
 import langList from "../langList";
+import {createRouter, createWebHistory} from "vue-router";
+import MakePage from "./pages/MakePage.vue";
+import IndexPage from "./pages/IndexPage.vue";
+import SearchPage from "./pages/SearchPage.vue";
+import QueryPage from "./pages/QueryBigData.vue";
+import QueryDetails from "./pages/QueryDetails.vue";
+import NotFound from "./pages/NotFound.vue";
 
-let i18n;
-// fetch("/get/site",{
-//     method:"POST"
-// }).then(res => res.json()).then(data=>{
-//     let lang = data.lang;
-//     i18n = new createI18n({
-//         locale: lang,
-//         messages:{
-//             en,
-//             zh,
-//         }
-//     })
-//
-// }).catch(()=>{
-i18n = new createI18n({
-    locale: window.site.lang,
-    messages: langList
-})
-// })
-const app = createApp(App);
-app.use(router);
-app.use(VueCookies);
-app.use(i18n)
-app.mount('#app');
+window.request("test")
+window.iForReq = false
+window.req("test")
 
-// const app =createApp(App);
-// app.use(routerSetting);
-// app.use(VueCookies);
-// app.mount('#app');
-// Dev mode
+let inv = setInterval(() => {
+    console.log("asd1")
+
+    if (window.iForReq) {
+        console.log("asd")
+        clearInterval(inv)
+        const i18n = new createI18n({
+            locale: window.site.lang,
+            messages: langList
+        })
+
+        const router = createRouter({
+            // history:createWebHashHistory(),
+            history: createWebHistory(),
+            routes: [{
+                path: "/make",
+                component: MakePage,
+                meta: {
+                    title: window.site.name + " | " + getI18n("pageMakeTitle")
+                },
+            }, {
+                path: "/",
+                component: IndexPage,
+            }, {
+                path: "/search",
+                component: SearchPage,
+                meta: {
+                    title: window.site.name + " | " + getI18n("pageSearchTitle")
+                },
+            }, {
+                path: "/queryBigData",
+                component: QueryPage,
+                meta: {
+                    title: window.site.name + " | " + getI18n("pageQueryBigData")
+                },
+            }, {
+                path: "/queryDetails",
+                component: QueryDetails,
+                meta: {
+                    title: window.site.name + " | " + getI18n("pageQueryDetails")
+                },
+            }, {
+                path: "/:any",
+                component: NotFound,
+                meta: {
+                    title: window.site.name + " | " + getI18n("pageNotFoundTitle")
+                },
+            }]
+        });
+
+        router.beforeEach((to, from, next) => {//beforeEach是router的钩子函数，在进入路由前执行
+            if (to.meta.title) {
+                document.title = to.meta.title
+            }
+            next()
+        })
+        const app = createApp(App);
+        app.use(router);
+        app.use(VueCookies);
+        app.use(i18n)
+        app.mount('#app');
+    }
+}, 100)
+
+
+function getI18n(title) {
+    return langList[window.site.lang][title]
+}
--- src/routerSetting/index.js
+++ src/routerSetting/index.js
@@ -1,60 +1,60 @@
-import {createRouter, createWebHistory} from "vue-router";
-import MakePage from "../pages/MakePage.vue";
-import IndexPage from "../pages/IndexPage.vue";
-import SearchPage from "../pages/SearchPage.vue";
-import QueryPage from "../pages/QueryBigData.vue";
-import QueryDetails from "../pages/QueryDetails.vue";
-import NotFound from "../pages/NotFound.vue";
-import langList from "../../langList";
-
-function getI18n(title) {
-    return langList[window.site.lang][title]
-}
-
-const router = createRouter({
-    // history:createWebHashHistory(),
-    history: createWebHistory(),
-    routes: [{
-        path: "/make",
-        component: MakePage,
-        meta: {
-            title: window.site.name + " | " + getI18n("pageMakeTitle")
-        },
-    }, {
-        path: "/",
-        component: IndexPage,
-    }, {
-        path: "/search",
-        component: SearchPage,
-        meta: {
-            title: window.site.name + " | " + getI18n("pageSearchTitle")
-        },
-    }, {
-        path: "/queryBigData",
-        component: QueryPage,
-        meta: {
-            title: window.site.name + " | " + getI18n("pageQueryBigData")
-        },
-    }, {
-        path: "/queryDetails",
-        component: QueryDetails,
-        meta: {
-            title: window.site.name + " | " + getI18n("pageQueryDetails")
-        },
-    }, {
-        path: "/:any",
-        component: NotFound,
-        meta: {
-            title: window.site.name + " | " + getI18n("pageNotFoundTitle")
-        },
-    }]
-});
-
-router.beforeEach((to, from, next) => {//beforeEach是router的钩子函数，在进入路由前执行
-    if (to.meta.title) {
-        document.title = to.meta.title
-    }
-    next()
-})
-
-export default router
+// import {createRouter, createWebHistory} from "vue-router";
+// import MakePage from "../pages/MakePage.vue";
+// import IndexPage from "../pages/IndexPage.vue";
+// import SearchPage from "../pages/SearchPage.vue";
+// import QueryPage from "../pages/QueryBigData.vue";
+// import QueryDetails from "../pages/QueryDetails.vue";
+// import NotFound from "../pages/NotFound.vue";
+// import langList from "../../langList";
+//
+// function getI18n(title) {
+//     return langList[window.site.lang][title]
+// }
+//
+// const router = createRouter({
+//     // history:createWebHashHistory(),
+//     history: createWebHistory(),
+//     routes: [{
+//         path: "/make",
+//         component: MakePage,
+//         meta: {
+//             title: window.site.name + " | " + getI18n("pageMakeTitle")
+//         },
+//     }, {
+//         path: "/",
+//         component: IndexPage,
+//     }, {
+//         path: "/search",
+//         component: SearchPage,
+//         meta: {
+//             title: window.site.name + " | " + getI18n("pageSearchTitle")
+//         },
+//     }, {
+//         path: "/queryBigData",
+//         component: QueryPage,
+//         meta: {
+//             title: window.site.name + " | " + getI18n("pageQueryBigData")
+//         },
+//     }, {
+//         path: "/queryDetails",
+//         component: QueryDetails,
+//         meta: {
+//             title: window.site.name + " | " + getI18n("pageQueryDetails")
+//         },
+//     }, {
+//         path: "/:any",
+//         component: NotFound,
+//         meta: {
+//             title: window.site.name + " | " + getI18n("pageNotFoundTitle")
+//         },
+//     }]
+// });
+//
+// router.beforeEach((to, from, next) => {//beforeEach是router的钩子函数，在进入路由前执行
+//     if (to.meta.title) {
+//         document.title = to.meta.title
+//     }
+//     next()
+// })
+//
+// export default router
