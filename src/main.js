f219d45d60024be09c70eb8af43813357437bcff
liangmiQwQ <mi_yyds@126.com>
2023-04-03 19:54:53 +0800

    perf: faster load page

--- src/main.js
+++ src/main.js
@@ -5,20 +5,46 @@ import VueCookies from 'vue-cookies'
 import {getRouter} from "./configs/router";
 import {getI18n} from "./configs/i18n";
 
-window.iForReq = false
-window.req("test")
+request("test",()=>{
+    const i18n = getI18n(window.site)
+    const router = getRouter(window.site)
 
-let inv = setInterval(() => {
-    if (window.iForReq) {
-        clearInterval(inv)
+    const app = createApp(App);
+    app.use(router);
+    app.use(VueCookies);
+    app.use(i18n)
+    app.mount('#app');
+})
 
-        const i18n = getI18n(window.site)
-        const router = getRouter(window.site)
-
-        const app = createApp(App);
-        app.use(router);
-        app.use(VueCookies);
-        app.use(i18n)
-        app.mount('#app');
+function request(mode,callback) {
+    if (mode === "test") {
+        let site = {
+            message: "success",
+            name: "米卷",
+            link: "https://localhost:3000/",
+            logo: "/favicon.png",
+            mainColor: "rgb(21, 127, 248)",
+            icp: "A nice questionnaire system",
+            lang: "zh",
+            needIcp: 0
+        }
+        // site = {}
+        setTimeout(() => {
+            siteChanger(site)
+        }, 500)
+        //simulation ping value
+    } else {
+        fetch("/get/site", {
+            method: "POST"
+        }).then(res => res.json()).then(site => {
+            siteChanger(site)
+        })
     }
-}, 100)
+    function siteChanger(site){
+        document.getElementById("favicon").href = site.logo;
+        document.documentElement.lang = site.lang;
+        window.site = site;
+        document.getElementById("titleForIndex").innerHTML = site.name;
+        callback()
+    }
+}
