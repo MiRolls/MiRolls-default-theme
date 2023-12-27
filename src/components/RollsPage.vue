5075bc0afedb5c02790c2415d76251f2a96b9e5e
liangmiQwQ <mi_yyds@126.com>
2023-12-28 06:13:48 +0800

    feat: draggable

--- src/components/RollsPage.vue
+++ src/components/RollsPage.vue
@@ -1,35 +1,40 @@
 <template>
     <div id="rollsPage" class="animate__animated animate__bounceInUp">
         <h2 class="TitleRollsT">{{ rolls.title }}</h2>
-        <div class="topic" v-for="(item,index) in rolls.quest" :key="index">
-            <span>{{ index + 1 }}.  </span>
-            <input class="questTitle" :placeholder="$t('makeQuestBlankTitleNormal')" type="text"
-                   v-model="item.title">
-            <input v-if="item.type === 'blank' || item.type === 'manyBlank'" class="questPlaceholder"
-                   v-model="item.placeholder" :placeholder="$t('makeQuestTips')">
-            <div v-if="item.type === 'choice' || item.type === 'radio'" class="options">
-                <div v-for="(options,i) in item.optionsNumber" :key="options + i +111">
-                    <span>{{ i + 1 }}.</span>
-                    <input :placeholder="$t('makeOptions')+(i+1)" class="option"
-                           v-model="item.options[i]" type="text">
+        <vue-draggable v-model="rolls.quest">
+            <div class="topic" v-for="(item,index) in rolls.quest" :key="index">
+                <span>{{ index + 1 }}.  </span>
+                <input class="questTitle" :placeholder="$t('makeQuestBlankTitleNormal')" type="text"
+                       v-model="item.title">
+                <input v-if="item.type === 'blank' || item.type === 'manyBlank'" class="questPlaceholder"
+                       v-model="item.placeholder" :placeholder="$t('makeQuestTips')">
+                <div v-if="item.type === 'choice' || item.type === 'radio'" class="options">
+                    <div v-for="(options,i) in item.optionsNumber" :key="options + i +111">
+                        <span>{{ i + 1 }}.</span>
+                        <input :placeholder="$t('makeOptions')+(i+1)" class="option"
+                               v-model="item.options[i]" type="text">
+                    </div>
+                    <!--               type="text" @input="changeQuestValue(item.options,i,$event.target.value)">-->
                 </div>
-                <!--               type="text" @input="changeQuestValue(item.options,i,$event.target.value)">-->
+                <button class="delButton" :title="$t('makeDelete')" @click="deleteQuest($event,index)">
+                    <svg fill="red" style="width: 1em;height: 1em" viewBox="0 0 1025 1024">
+                        <path
+                            d="M718.882684 511.351282 1010.410118 800.927611C1024.685139 815.107052 1027.762671 835.153596 1017.28356 845.70286L849.127929 1014.985224C838.649124 1025.534489 818.582055 1022.591441 804.307034 1008.412L512.781438 718.837509 223.267604 1010.296628C209.089387 1024.570118 189.044682 1027.647038 178.496643 1017.169458L9.230209 849.03282C-1.318137 838.554934 1.624604 818.49001 15.802821 804.216827L305.314511 512.759546 13.70467 223.101423C-0.570351 208.921981-3.647577 188.875438 6.831534 178.326173L174.986859 9.043809C185.46597-1.505455 205.533039 1.437592 219.80806 15.617034L511.415756 305.273319 801.039568 13.703302C815.217785-0.570187 835.26249-3.647107 845.810529 6.83078L1015.076963 174.967417C1025.625309 185.444997 1022.682568 205.509921 1008.504351 219.78341L718.882684 511.351282Z"></path>
+                    </svg>
+                </button>
+                <!-- v-if来实现各种题目的效果 -->
             </div>
-            <button class="delButton" :title="$t('makeDelete')" @click="deleteQuest($event,index)">
-                <svg fill="red" style="width: 1em;height: 1em" viewBox="0 0 1025 1024">
-                    <path
-                        d="M718.882684 511.351282 1010.410118 800.927611C1024.685139 815.107052 1027.762671 835.153596 1017.28356 845.70286L849.127929 1014.985224C838.649124 1025.534489 818.582055 1022.591441 804.307034 1008.412L512.781438 718.837509 223.267604 1010.296628C209.089387 1024.570118 189.044682 1027.647038 178.496643 1017.169458L9.230209 849.03282C-1.318137 838.554934 1.624604 818.49001 15.802821 804.216827L305.314511 512.759546 13.70467 223.101423C-0.570351 208.921981-3.647577 188.875438 6.831534 178.326173L174.986859 9.043809C185.46597-1.505455 205.533039 1.437592 219.80806 15.617034L511.415756 305.273319 801.039568 13.703302C815.217785-0.570187 835.26249-3.647107 845.810529 6.83078L1015.076963 174.967417C1025.625309 185.444997 1022.682568 205.509921 1008.504351 219.78341L718.882684 511.351282Z"></path>
-                </svg>
-            </button>
-            <!-- v-if来实现各种题目的效果 -->
-        </div>
+        </vue-draggable>
         <!--        <div class="line"></div>-->
     </div>
 </template>
 <!--suppress JSUnresolvedVariable -->
 <script>
+import {VueDraggable} from "vue-draggable-plus";
+
 export default {
     name: "RollsPage",
+    components: {VueDraggable},
     data() {
         return {
             rolls: {
@@ -81,17 +86,17 @@ export default {
             setTimeout(() => {
                 console.log(target.parentElement);
                 target.parentElement.style.marginTop = target.parentElement.offsetHeight + "px" // ad normally margin
-                setTimeout(()=>target.parentElement.classList.add("transition-halfS")/* add transition class */,10)
+                setTimeout(() => target.parentElement.classList.add("transition-halfS")/* add transition class */, 10)
                 // target.parentElement.classList.add("transition-halfS")
 
                 this.rolls.quest.splice(index, 1)
                 target.parentElement.classList.remove("animate__zoomOut")
                 target.parentElement.classList.remove("animate__animated")
             }, 360)
-            setTimeout(()=>{
+            setTimeout(() => {
                 target.parentElement.style.margin = null // ad normally margin
-                setTimeout(()=>target.parentElement.classList.remove("transition-halfS"),550)
-            },390)
+                setTimeout(() => target.parentElement.classList.remove("transition-halfS"), 550)
+            }, 390)
             let target = event.target
             while (target.tagName !== "BUTTON") {
                 target = target.parentElement
@@ -148,7 +153,7 @@ export default {
 }
 </script>
 <style>
-.transition-halfS{
+.transition-halfS {
     transition: 0.5s;
 }
 
@@ -174,6 +179,7 @@ export default {
     height: 18px;
     margin-top: 10px;
     width: 50%;
+    background: none;
 //display: block;
 }
 
@@ -189,6 +195,7 @@ export default {
     height: 18px;
     /*margin-top: 10px;*/
     width: calc(100% - 25px);
+    background: none;
     /*display: block;*/
 }
 
@@ -207,8 +214,7 @@ export default {
     padding: 10px 30px 10px 30px;
     position: relative;
     border-bottom: 1px solid #bebebe;
-    //transition: 0.5s;
-//border-top: 1px solid #bebebe; text-align: left;
+//transition: 0.5s; //border-top: 1px solid #bebebe; text-align: left;
 }
 
 #rollsPage {
