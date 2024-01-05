2d74870b309bd581d23f8cba94f96423eed53cef
liangmiQwQ <mi_yyds@126.com>
2024-01-05 18:51:11 +0800

    feat: change make_roll steps to i18n

--- src/configs/tutorial/make_roll.js
+++ src/configs/tutorial/make_roll.js
@@ -1,4 +1,5 @@
 import {driver} from 'driver.js'
+import LangList from "../../../langList";
 
 const make_roll = () => {
     const config = driver({
@@ -7,34 +8,37 @@ const make_roll = () => {
             {
                 element: "#MakePageLeftControl",
                 popover: {
-                    title: "添加题目",
-                    description: "你可以通过左边的控制栏来添加题目"
+                    title: getI18n("makePageTutorial_1stTitle"),
+                    description: getI18n("makePageTutorial_1sDescription")
                 }
             },
             {
                 element: ".topic",
                 popover: {
-                    title: "编辑题目",
-                    description: "你可以通过这些项目来更改你的题目的信息，例如选项名称等"
+                    title: getI18n("makePageTutorial_2ndTitle"),
+                    description: getI18n("makePageTutorial_2ndDescription")
                 }
             },
             {
                 element: ".delButton",
                 popover: {
-                    title: "删除题目",
-                    description: "你可以点击这个按钮来删除一个题目"
+                    title: getI18n("makePageTutorial_3rdTitle"),
+                    description: getI18n("makePageTutorial_3rdDescription")
                 }
             },
             {
                 element: ".bottomControl",
                 popover: {
-                    title: "提交与标题",
-                    description: "在这里你可以编辑你的标题，也可以设置保存为草稿箱/提交问卷"
+                    title: getI18n("makePageTutorial_4thTitle"),
+                    description: getI18n("makePageTutorial_4thDescription")
                 }
-            }
+            },
         ]
     })
     config.drive()
 }
 
+function getI18n(tagName){
+    return LangList[window.site.lang][tagName]
+}
 export default make_roll
