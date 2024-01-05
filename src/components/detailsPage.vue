9812ff8f6a487746341b0b1d8e02ac5c447ec13c
liangmiQwQ <mi_yyds@126.com>
2024-01-05 21:45:04 +0800

    style: changes $details style

--- src/components/detailsPage.vue
+++ src/components/detailsPage.vue
@@ -13,13 +13,17 @@ export default {
 <style scoped>
 
 #details {
-    margin-top: 73px;
-    margin-left: 15%;
+    margin-top: calc(73px + 70px);
+    margin-left: calc(15% + 65px);
     min-height: calc(100vh - 229px);
+    width: calc(85% - 120px);
+    border-radius: 12px;
+    box-shadow: rgba(27, 31, 35, 0.34) 2px 1px 12px;
+    margin-bottom: 70px;
 }
 
 .answers {
-    width: 100%;
+    width: calc(100% * 0.85);
     height: 40px;
     padding: 10px 30px;
     border-bottom: #b7b7b7 solid 1px;
